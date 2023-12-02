3ef9ee3aa4fd8c485123681ff5b1a5cefd4f16a3
HIRATA Soma <e9248@g.maizuru-ct.ac.jp>
2023-12-02 15:42:35 +0900

    feat: YOLOを用いたリアルタイム画像認識処理機能を追加

--- TeamMission/empty.py
+++ TeamMission/empty.py
@@ -1,5 +1,29 @@
 from ultralytics import YOLO
+import cv2
 
-model: YOLO = YOLO(model="yolov8.pt")
+# モデル読み込み
+model = YOLO(R"C:\Users\hirat\Documents\OpenCV\TeamMission\yolov8n.pt")
 
-result: list = model.predict("https://ultralytics.com/images/bus.jpg",)
+# カメラ読み込み
+cap = cv2.VideoCapture(0)
+
+while True:
+    # カメラから1フレーム読み込み
+    ret, frame = cap.read()
+
+    # YOLOv8で物体検出
+    results = model(frame, show=False)
+
+    # 結果を表示
+    for result in results:
+        for xyxy in result.boxes.xyxy:
+            cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (0, 0, 255), 2)
+    cv2.imshow('frame', frame)
+
+    # qキーで終了
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 後処理
+cap.release()
+cv2.destroyAllWindows()
